741c4b4bb92fd1035e46fe414452e2f049f34739
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-10-24 22:39:50 -0600

    ugh

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -148,6 +148,4 @@ LOGIN_URL = '/login/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+DATABASES["default"].update(db_from_env)
