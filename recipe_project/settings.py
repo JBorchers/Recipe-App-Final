69ee49c117b37f12aa53ab2a9427dc268a118d80
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-11-01 11:00:48 -0600

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -149,7 +149,7 @@ LOGIN_URL = '/login/'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 print(os.environ.get('DATABASE_URL'))
