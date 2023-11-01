28789e9f03c9ab0376ea783e5c651364dc04c5bf
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-11-01 15:47:43 -0600

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "pacific-retreat-90795-95b4534c0354.herokuapp.com",
