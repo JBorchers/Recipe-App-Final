2e1335d4e4f90362fb92a9766167cc8666c292c7
JBorchers <79878454+JBorchers@users.noreply.github.com>
2023-11-04 23:01:35 -0600

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -18,12 +18,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 # Read the secret key from the environment variable
+
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 ALLOWED_HOSTS = [
     "*"
 ]
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
